e545540baeecfdfe69b8f18b48e5e78dff652d9d
Александр <34779498+Canek4005@users.noreply.github.com>
2022-10-19 00:56:13 +0300

    change to cp

--- devops/prod/deploy.bash
+++ devops/prod/deploy.bash
@@ -1,11 +1,11 @@
 # Перекладываем статику
 sudo rm -rf /usr/share/nginx/html/*
 sudo mkdir -p /usr/share/nginx/html
-sudo mv -rf ~/build/* /usr/share/nginx/html
+sudo cp -rf ~/build/* /usr/share/nginx/html
 # Перекладываем конфигурационный файл
 sudo rm -rf /etc/nginx/*
 sudo mkdir -p /etc/nginx
-sudo mv -rf ~/prod/nginx.conf /etc/nginx
+sudo cp -rf ~/prod/nginx.conf /etc/nginx
 
 # Запускаем сервис
 docker-compose up --force-recreate --build -d
