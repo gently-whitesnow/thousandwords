8e1914b8a92d81af2fbe770768c5c40f88350956
Александр <34779498+Canek4005@users.noreply.github.com>
2022-10-21 23:24:23 +0300

    add comments

--- devops/dev/deploy.bash
+++ devops/dev/deploy.bash
@@ -4,3 +4,7 @@ npm run build --prefix ~/thousandwords/frontend # TEST
 # Запускаем сервис
 docker-compose up --force-recreate --build -d
 docker image prune -f
+
+
+# sudo find / -name "*sshd_config*"
+# sudo chown -R ci:cicd /etc/nginx/ 
--- devops/prod/deploy.bash
+++ devops/prod/deploy.bash
@@ -1,20 +1,21 @@
-# Перекладываем статику
+# Перекладываем статику в монтируемое nginx место
 rm -rf /usr/share/nginx/html/*
 mkdir -p /usr/share/nginx/html
 cp -rf ~/build/* /usr/share/nginx/html
-# Перекладываем конфигурационный файл
+# Перекладываем конфигурационный файл для nginx
 rm -rf /etc/nginx/*
 mkdir -p /etc/nginx
 cp -rf ~/prod/nginx.conf /etc/nginx
 
-# логинимся в докере
+# Логинимся в докере, чтобы при запуске yml файла
+# была возможность скачать наш контейнер с Docker Hub
+# DOCKER_LOGIN и DOCKER_PWD/DOCKER_ACCESS_TOKEN необходимо заранее задать в 
+# environment сервера
 docker login https://index.docker.io/v2 -u $DOCKER_LOGIN -p $DOCKER_PWD 
 
-# Запускаем сервис
-docker-compose up --force-recreate --build -d
+# Запускаем сервис (перезапускаем в фоновом режиме)
+docker-compose up --force-recreate -d
 docker image prune -f
 
 
 
-# sudo find / -name "*sshd_config*"
-# sudo chown -R ci:cicd /etc/nginx/ 
